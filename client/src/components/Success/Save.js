d099fc2885f4a87e8f4eb746f0ba69529474654a
RetroAstro <452461114@qq.com>
2018-12-16 18:07:29 +0800

    add html2canvas & qrcode plugin

--- client/src/components/Success/Save.js
+++ client/src/components/Success/Save.js
@@ -1,21 +1,45 @@
-import React from 'react'
+import React, { Component } from 'react'
+import html2canvas from 'html2canvas'
 
-const Save = () => (
-  <div className="save-wrap flex-center">
-    <div className="save-box flex-col-between">
-      <div className="qr-code bg-cover-all">
+class Save extends Component {
+  state = {
+    dataURL: ''
+  }
+  htmltoImage () {
+    html2canvas(this.dom)
+      .then((canvas) => {
+        var context = canvas.getContext('2d')
+        context.imageSmoothingEnabled = false
+        this.setState({
+          dataURL: canvas.toDataURL()
+        })
+      })
+  }
+  componentDidMount () {
+    this.htmltoImage()
+  }
+  render () {
+    return (
+      <div className="save-wrap flex-center">
+        <div
+          className="save-box flex-col-between"
+          ref={el => { this.dom = el }}
+        >
+          <div className="qr-code bg-cover-all">
+          </div>
+          <div className="save-btn">
+            <span className="translate-center">
+            点击保存
+            </span>
+            <a
+              href={this.state.dataURL}
+              download={this.state.dataURL}
+            />
+          </div>
+        </div>
       </div>
-      <div className="save-btn">
-        <span className="translate-center">
-          点击保存
-        </span>
-        <a
-          href="test.jpg"
-          download="test.jpg"
-        />
-      </div>
-    </div>
-  </div>
-)
+    )
+  }
+}
 
 export default Save
